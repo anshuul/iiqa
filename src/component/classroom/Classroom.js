b6d96568daf5af63b302d1b7f396bdc2c4308730
anshuul <anshultated1999@gmail.com>
2021-02-18 14:22:25 +0530

    added create class and join class buttons

--- src/component/classroom/Classroom.js
+++ src/component/classroom/Classroom.js
@@ -1,31 +1,73 @@
-    import React , { Component } from "react";
-    import Classes from "./classes/Classes";
+import React, { Component } from "react";
+import Classes from "./classes/Classes";
 
-    class Classroom extends Component {
-    
-        state = {
-            classroom: [
-                {id: '1', title: 'A'},
-                {id: '2', title: 'B'},
-                {id: '3', title: 'C'}
-            ]
-            
-        }
-        
-        render () {
-            const classrooms = this.state.classroom;
-            console.log(classrooms);
-            return (
-                <div className="conatainer">
-                    {classrooms.map(classroom => {
-                        return (
-                            <Classes classroom={classroom} key={classroom.id}/>
-                        )
-                    })}
-                </div>
-            )
-            
-        }
-    }
+class Classroom extends Component {
+  state = {
+    classroom: [
+      { id: "1", title: "A" },
+      { id: "2", title: "B" },
+      { id: "3", title: "C" },
+      { id: "1", title: "A" },
+      { id: "2", title: "B" },
+      { id: "1", title: "A" },
+      { id: "2", title: "B" },
+      { id: "1", title: "A" },
+      { id: "2", title: "B" },
+    ],
+  };
+  buttonHandler = () => {
+    console.log("Button Clicked");
+  };
 
-    export default Classroom;
+  render() {
+    const classrooms = this.state.classroom;
+    console.log(classrooms);
+    return (
+      <div className="conatainer">
+        <div
+          className="buttonclass"
+          style={{ display: "flex", justifyContent: "center", width: "100%" }}
+        >
+          <button
+            onClick={this.buttonHandler}
+            className="selection blue darken-2 btn-flat btn-large"
+            style={{
+              color: "white",
+              borderRadius: "20px",
+              marginTop: "20px",
+              width: "20%",
+              fontSize: 20,
+              marginLeft: "20px",
+              marginRight: "20px",
+              marginBottom: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            Create
+          </button>
+          <button
+            onClick={this.buttonHandler}
+            className="red darken-2 btn-flat btn-large"
+            style={{
+              color: "white",
+              borderRadius: "20px",
+              marginTop: "20px",
+              width: "20%",
+              fontSize: 20,
+              marginBottom: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            Join
+          </button>
+        </div>
+
+        {classrooms.map((classroom) => {
+          return <Classes classroom={classroom} key={classroom.id} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Classroom;
--- src/component/classroom/classes/Classes.js
+++ src/component/classroom/classes/Classes.js
@@ -1,13 +1,41 @@
 import React from "react";
 
+const colors = ["blue", "teal", "red", "green", "orange"];
 const Classes = (props) => {
-    return ( 
-        <div className="parent" style={{height: "250px", width: "190px", border:"1px solid black", boxShadow: "10px 10px 10px grey", marginTop:'10px', marginLeft:'10px'}}>
-            <div className="child  teal lighten-2" style={{margin:'0px', height:'80px'}}>
-                <p style={{margin: '0px', paddingTop:'5px', paddingLeft:'5px'}}>{props.classroom.title}</p>
-            </div>
+  const defaultClassName = "child lighten-2";
+  const randomize = () => {
+    return colors[Math.floor(Math.random() * 5)];
+  };
+  const finalClassName = `${defaultClassName} ${randomize()}`;
+  return (
+    <center>
+      <div
+        className="parent"
+        style={{
+          height: "250px",
+          width: "190px",
+          border: "1px solid grey",
+          boxShadow: "10px 10px 10px grey",
+          marginTop: "10px",
+          marginLeft: "10px",
+          borderRadius: "10px",
+        }}
+      >
+        <div
+          className={finalClassName}
+          style={{
+            margin: "0px",
+            height: "80px",
+            borderRadius: "10px 10px 0px 0px",
+          }}
+        >
+          <p style={{ margin: "0px", paddingTop: "5px", paddingLeft: "5px" }}>
+            {props.classroom.title}
+          </p>
         </div>
-     ); 
-}
+      </div>
+    </center>
+  );
+};
 
 export default Classes;
