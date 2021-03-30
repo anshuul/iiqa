2724568eba4324072cd6020a6b57fe5f7ad7adb2
Dishank Tak <39084848+Dishank99@users.noreply.github.com>
2021-03-30 15:17:02 +0530

    fixed issue of avatars popping in class compo

--- src/component/layout/Avatar/index.js
+++ src/component/layout/Avatar/index.js
@@ -4,7 +4,7 @@ import './index.css'
 export default function index(props) {
     return (
         <div className={`displayPicture ${props.className}`} >
-            <img src={props.displayPicture} alt='displayPicture' style={{width:'100%', height:'100%'}}/>
+            <img src={props.displayPicture} alt='displayPicture' width='60px' height='60px'/>
         </div>
     )
 }
