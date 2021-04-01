ef20563e454f04b4df964e00cdfee36ebb26a704
Dishank Tak <39084848+Dishank99@users.noreply.github.com>
2021-04-01 13:48:46 +0530

    fixed avatar style

--- src/component/layout/Avatar/index.js
+++ src/component/layout/Avatar/index.js
@@ -4,7 +4,7 @@ import './index.css'
 export default function index(props) {
     return (
         <div className={`displayPicture ${props.className}`} >
-            <img src={props.displayPicture} alt='displayPicture' width='60px' height='60px'/>
+            <img src={props.displayPicture} alt='displayPicture' width='97%' height='97%'/>
         </div>
     )
 }
